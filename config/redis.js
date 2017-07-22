6131b37cb5035928182c768e224f15876628f383
Adam Karbiener <adamkarb@gmail.com>
2017-07-22 15:47:48 -0400

    Updating some configs

--- config/redis.js
+++ config/redis.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const REDIS_URL = process.env.REDISCLOUD_URL || process.env.REDIS_URL;
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 
 var redisPromise = require('promise-redis')();
 
 var redis = redisPromise.createClient(REDIS_URL);
 
-redis.on('error' , (err) => {
+redis.on('error', (err) => {
 
 	console.log(`An error has occurred with the redis database connection: ${err.stack}`);
 
 });
 
-redis.on('ready' , () => {
+redis.on('ready', () => {
 
 	console.log(`:: Connected to redis database @ ${REDIS_URL} ::`);
 
